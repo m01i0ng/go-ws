cd67c0a4766d672a3e50b83838b749bb9ce3c7d7
m01i0ng <alley.ma@qq.com>
2020-01-05 22:40:23 +0800

    rename

--- example/server.go
+++ example/server.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-	"net/http"
-	"time"
+    "net/http"
+    "time"
 
-	"github.com/gorilla/websocket"
-	"github.com/kataras/golog"
-	"github.com/m01i0ng/go-ws"
+    "github.com/gorilla/websocket"
+    "github.com/kataras/golog"
+    "github.com/m01i0ng/go-ws"
 )
 
 var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
 }
 
 func main() {
-	http.HandleFunc("/ws", wsHandler)
-	golog.Info("server starting on :8888")
-	err := http.ListenAndServe(":8888", nil)
-	if err != nil {
-		golog.Errorf("err: %s", err)
-	}
+    http.HandleFunc("/ws", wsHandler)
+    golog.Info("server starting on :8888")
+    err := http.ListenAndServe(":8888", nil)
+    if err != nil {
+        golog.Errorf("err: %s", err)
+    }
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		golog.Errorf("upgrade error: %s", err)
-	}
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        golog.Errorf("upgrade error: %s", err)
+    }
 
-	golog.Infof("new conn: %s", conn.RemoteAddr().String())
+    golog.Infof("new conn: %s", conn.RemoteAddr().String())
 
-	connection := go_ws.NewConnection(conn)
+    connection := go_ws.NewConnection(conn)
 
-	go func() {
-		for {
-			err := connection.Write([]byte("heart"))
-			if err != nil {
-				return
-			}
+    go func() {
+        for {
+            err := connection.Write([]byte("heart"))
+            if err != nil {
+                return
+            }
 
-			time.Sleep(time.Second)
-		}
-	}()
+            time.Sleep(time.Second)
+        }
+    }()
 
-	for {
-		bytes, err := connection.Read()
-		if err != nil {
-			_ = connection.Close()
-			break
-		}
+    for {
+        bytes, err := connection.Read()
+        if err != nil {
+            _ = connection.Close()
+            break
+        }
 
-		err = connection.Write(bytes)
-		if err != nil {
-			_ = connection.Close()
-			break
-		}
-	}
+        err = connection.Write(bytes)
+        if err != nil {
+            _ = connection.Close()
+            break
+        }
+    }
 }
